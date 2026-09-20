2a791c37a5cb9aa7a3efc6bb14d3a6e6b241e7bf
initialyannick-tech <initialyannick@gmail.com>
2026-09-20 04:12:05 +0100

    model

--- Modules/Pedagogie/app/Models/Eleve.php
+++ Modules/Pedagogie/app/Models/Eleve.php
@@ -5,7 +5,8 @@ namespace Modules\Pedagogie\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+//use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Str;
 
 
 class Eleve extends Model
@@ -36,6 +37,27 @@ class Eleve extends Model
     protected $casts = [
         'date_naissance' => 'date',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($eleve) {
+
+            $anneeNaissance = \Carbon\Carbon::parse($eleve->date_naissance)->format('Y');
+            $initialeNom = strtoupper(substr(Str::ascii(trim($eleve->nom)), 0, 1));
+            $initialePrenom = strtoupper( substr(Str::ascii(trim($eleve->prenom)), 0, 1));
+            $dernier = Eleve::where('matricule', 'like', "{$anneeNaissance}-{$initialeNom}{$initialePrenom}-%")->orderByDesc('id')->first();
+            $numero = 1;
+            if ($dernier) {
+                preg_match('/-(\d+)$/', $dernier->matricule, $matches);
+                if (isset($matches[1])) {
+                    $numero = ((int) $matches[1]) + 1;
+                }
+            }
+            $eleve->matricule = sprintf('%s-%s%s-%03d', $anneeNaissance,$initialeNom, $initialePrenom, $numero
+            );
+        });
+    }
 
     /**
      * Un élève appartient à un seul parent/tuteur.
